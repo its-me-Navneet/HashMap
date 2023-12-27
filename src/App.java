1e2b188dfd772bd6a0158e34f76a7df60806075d
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-27 21:01:17 +0530

    Added UC1

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        String randomText ="I go do go hey we go for we go for we shall we " ; 
+
+        HashMap<String,Integer> countWord = new HashMap<>() ;
+
+        String arr[] = randomText.split(" "); 
+
+        for(String word :arr) {
+             countWord.put(word,countWord.getOrDefault(word,1) +1) ;
+        }
+
+        for(Map.Entry m : countWord.entrySet() ){
+              
+            System.out.println(m.getKey() + " " + m.getValue());
+
+        } 
+
+    }
+}
