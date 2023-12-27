c330e17f1f8d63454e8f3daf2ce2c1ad30d572a2
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-27 21:27:31 +0530

    Added UC3

--- src/App.java
+++ src/App.java
@@ -6,11 +6,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         
 
-        String randomText ="I go do go hey we go for we go for we shall we " ; 
+        String randomText = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations" ; 
 
         HashMap<String,Integer> countWord = new HashMap<>() ;
 
-        String arr[] = randomText.split(" "); 
+        String arr[] = randomText.replaceFirst("avoidable","").split(" "); 
 
         for(String word :arr) {
              countWord.put(word,countWord.getOrDefault(word,1) +1) ;
